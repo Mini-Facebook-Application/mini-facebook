5d4e7d8c1ebd92c8e6bc2abcf90ec5e5bf50cfb7
hinnaparwezuc <parwezhs@mail.uc.edu>
2026-08-04 19:12:12 -0400

    SQL migration for users, posts, and comments tables

--- migration_sprint2.sql
+++ migration_sprint2.sql
@@ -1,20 +1,33 @@
 USE minifacebook;
 
 ALTER TABLE users
-    ADD COLUMN role ENUM('user', 'superuser') NOT NULL DEFAULT 'user',
-    ADD COLUMN is_disabled TINYINT(1) NOT NULL DEFAULT 0;
+    ADD COLUMN role
+        ENUM('user', 'superuser')
+        NOT NULL
+        DEFAULT 'user',
+
+    ADD COLUMN is_disabled
+        TINYINT(1)
+        NOT NULL
+        DEFAULT 0;
 
 ALTER TABLE posts
-    ADD COLUMN updated_at TIMESTAMP
-    DEFAULT CURRENT_TIMESTAMP
-    ON UPDATE CURRENT_TIMESTAMP;
+    ADD COLUMN updated_at
+        TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP;
 
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
+
     post_id INT NOT NULL,
+
     user_id INT NOT NULL,
+
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    created_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (post_id)
         REFERENCES posts(id)
