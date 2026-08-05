9d3ab74cfd6e731a317b89028f9c600fe91ce3bf
hinnaparwezuc <parwezhs@mail.uc.edu>
2026-08-05 01:53:48 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -78,3 +78,19 @@ CREATE TABLE comments (
         REFERENCES users(id)
         ON DELETE CASCADE
 );
+-- Store live chat messages.
+
+CREATE TABLE chat_messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    user_id INT NOT NULL,
+
+    message VARCHAR(500) NOT NULL,
+
+    created_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
